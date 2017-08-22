81828c991709afdcafc1c29cc5e408b6dc1f7196
Noémie Lehuby <noemie.lehuby@openmailbox.org>
2017-08-22 17:11:33 +0200

    lignes - affinage de l'affichage par niveau de zoom

--- web/index.js
+++ web/index.js
@@ -15,7 +15,7 @@ map.on('load', function() {
         var source_detail = osm_lines["sources"][source_name];
         map.addSource(source_name, source_detail);
         for (i in osm_lines["layers"]) {
-            map.addLayer(osm_lines["layers"][i]);            
+            map.addLayer(osm_lines["layers"][i]);
         }
     });
 
@@ -25,7 +25,7 @@ map.on('load', function() {
         map.getCanvas().style.cursor = 'pointer';
         // Single out the first found feature.
         var feature = e.features[0];
-        
+
         // Display a popup with the name
         popup.setLngLat(e.lngLat)
             .setText(feature.properties.name)
@@ -42,28 +42,29 @@ map.on('load', function() {
         popup.setLngLat(e.lngLat)
             .setText(feature.properties.name)
             .addTo(map);
-        
+
     });
 
 });
 
 map.on('render', function(e) {
-        if (map.loaded()) {        
-                var lines_list = document.getElementById('map-overlay');
-                lines_list.innerHTML = "Liste des lignes <br>";
-                
-                all_other_lines = map.queryRenderedFeatures({ layers: ['other_lines'] });
-                all_other_lines_names = all_other_lines.map(function(x){return x.properties.name;})
-                var dedup = all_other_lines_names.filter(function(item, pos, self) {
-                        return self.indexOf(item) == pos;
-                        });
-                
+    if (map.loaded()) {
+        var lines_list = document.getElementById('map-overlay');
+        lines_list.innerHTML = "Liste des lignes <br>";
 
-                for (i in dedup.sort()) {
-                    lines_list.innerHTML += dedup[i] + "<br>";            
-                }
+        all_other_lines = map.queryRenderedFeatures({
+            layers: ['other_lines']
+        });
+        all_other_lines_names = all_other_lines.map(function(x) {
+            return x.properties.name;
+        })
+        var dedup = all_other_lines_names.filter(function(item, pos, self) {
+            return self.indexOf(item) == pos;
+        });
+
+
+        for (i in dedup.sort()) {
+            lines_list.innerHTML += dedup[i] + "<br>";
         }
+    }
 });
-
-
-
